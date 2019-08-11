719ee10d93be1b669bb9dfa490308f4ea508efd0
relubwu <bwu@ucsd.edu>
2019-08-11 16:53:30 +0800

    Modified terms

--- pages/vitae/locale-package.js
+++ pages/vitae/locale-package.js
@@ -5,4 +5,4 @@ export const emptyCoupon = ['暂无优惠券可用哦～', 'No valid coupon foun
 export const reward = ['会员好礼', 'rewards'];
 export const register = ['成为VITA会员', 'Register'];
 export const link = ['绑定VITA卡', 'Link'];
-export const disclaimer = ['绑定或注册代表您已阅读并同意隐私协议和用户协议', 'Login or register remarks your consent to our Privacy Policy and EULA']
+export const disclaimer = ['绑定或注册代表您已阅读并同意隐私协议和用户协议', 'Login or register represents your consent to our Privacy Policy and EULA']
