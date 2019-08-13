14c27b42cc36f9a685092c6b81b7a59dac3c7694
relubwu <bwu@ucsd.edu>
2019-08-13 22:21:33 +0800

    Append new toast

--- utils/toasts.js
+++ utils/toasts.js
@@ -6,3 +6,10 @@ export const requestFailed = locale => {
     image: '/assets/icons/request-fail.png'
   });
 }
+
+export const incompleteForm = locale => {
+  wx.showToast({
+    title: GlobalLocalePackages.incompleteForm[locale],
+    icon: 'none'
+  })
+}
