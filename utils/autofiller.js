5e0aa7bd315c5866e1c5f1c6a7d811e137006609
relubwu <bwu@ucsd.edu>
2019-08-14 21:49:48 +0800

    Append 表单Autofiller

---
+++ utils/autofiller.js
@@ -0,0 +1,11 @@
+export default function autofiller (fields, feed) {
+  console.log(fields);
+  let result = {};
+  for (let field of fields) {
+    // Don't use boolean shorthand, gender & region contains [0, 1]
+    if (feed[field.alias] != null) {
+      result[field.key] = feed[field.alias];
+    }
+  }
+  return result;
+}
