138172dddafe508ef71e129d22b871cca02b4929
relubwu <bwu@ucsd.edu>
2019-08-16 13:03:31 +0800

    Update Major overhaul

--- pages/vendor-list/vendor-list.js
+++ pages/vendor-list/vendor-list.js
@@ -15,7 +15,13 @@ Page({
     LocalePackage,
     cursor: {},
     current: 0,
-    bottomFlag: []
+    bottomFlag: [],
+    search: {
+      keyword: '',
+      sort: {
+        criteria: NaN
+      }
+    }
   },
   onLoad: function (options) {
     let { locale } = Store.getState().global;
@@ -54,28 +60,30 @@ Page({
           categories
         });
         this.data.cursor[categories[0].name] = {
-          start: 0,
-          end: 10
+          skip: 0
         };
-        if (!this.data.category)
-          return request(API.VENDOR.LISTS, METHOD.GET, {
-            locale: Store.getState().global.locale,
-            realm: this.data.options.realm,
-            category: categories[0].name,
-            start: this.data.cursor[categories[0].name].start,
-            end: this.data.cursor[categories[0].name].end
-          });
+        this.fetchList(categories[0].name, 0, wx.hideLoading);
+      //   if (!this.data.category)
+      //     return request(API.VENDOR.LISTS, METHOD.GET, {
+      //       locale: Store.getState().global.locale,
+      //       realm: this.data.options.realm,
+      //       category: categories[0].name,
+      //       start: this.data.cursor[categories[0].name].start,
+      //       end: this.data.cursor[categories[0].name].end
+      //     });
+      // })
+      // .then(res => {
+      //   let vendors = { 
+      //     ...this.data.vendors,
+      //     ...res
+      //   };
+      //   this.setData({
+      //     vendors
+      //   });
+      //   wx.hideLoading();
+      // })
+      // .catch(e => Toasts.requestFailed(Store.getState().global.locale));
       })
-      .then(res => {
-        let vendors = { ...this.data.vendors,
-          ...res
-        };
-        this.setData({
-          vendors
-        });
-        wx.hideLoading();
-      })
-      .catch(e => Toasts.requestFailed(Store.getState().global.locale));
   },
   onHide: function () {
     this.unsubscribe();
@@ -89,67 +97,115 @@ Page({
     this.setData({
       current: index
     });
-    if (!this.data.vendors[this.data.categories[index].name])
-      wx.showLoading({});
-      if (!this.data.cursor[this.data.categories[index].name])
-        this.data.cursor[this.data.categories[index].name] = {
-          start: 0,
-          end: 10
-        };
-      request(API.VENDOR.LISTS, METHOD.GET, {
-          locale: this.data.locale,
-          realm: this.data.options.realm,
-          category: this.data.categories[index].name,
-          start: this.data.cursor[this.data.categories[index].name].start,
-          end: this.data.cursor[this.data.categories[index].name].end
-        })
-        .then(res => {
-          let vendors = { ...this.data.vendors,
-            ...res
-          };
-          this.setData({
-            vendors
-          });
-          wx.hideLoading();
-        });
+    let currentCategory = this.data.categories[index].name;
+    wx.showLoading({
+      title: GlobalLocalePackages.loading[this.data.locale]
+    });
+    if (!this.data.cursor[this.data.categories[index].name])
+      this.data.cursor[this.data.categories[index].name] = {
+        skip: 0,
+      };
+    this.fetchList(currentCategory, 0, wx.hideLoading);
+      // request(API.VENDOR.LISTS, METHOD.GET, {
+      //     locale: this.data.locale,
+      //     realm: this.data.options.realm,
+      //     category: this.data.categories[index].name,
+      //     start: this.data.cursor[this.data.categories[index].name].start,
+      //     end: this.data.cursor[this.data.categories[index].name].end
+      //   })
+      //   .then(res => {
+      //     let vendors = { 
+      //       ...this.data.vendors,
+      //       ...res
+      //     };
+      //     this.setData({
+      //       vendors
+      //     });
+      //     wx.hideLoading();
+      //   });
   },
   onReachBottom: function (e) {
     let currentCategory = this.data.categories[this.data.current].name;
     wx.showNavigationBarLoading();
     if (!!this.data.cursor[currentCategory]) {
-      this.data.cursor[currentCategory].start += 10;
-      this.data.cursor[currentCategory].end += 10;
+      this.data.cursor[currentCategory].skip += 10;
     }
     if (!!this.data.bottomFlag[currentCategory]) {
       wx.hideNavigationBarLoading();
       return;
     }
+    let { skip } = this.data.cursor[currentCategory];
+    this.fetchList(currentCategory, skip, wx.hideNavigationBarLoading);
+    // request(API.VENDOR.LISTS, METHOD.GET, {
+    //   locale: this.data.locale,
+    //   realm: this.data.options.realm,
+    //   category: currentCategory,
+    //   start: this.data.cursor[currentCategory].start,
+    //   end: this.data.cursor[currentCategory].end
+    // })
+    //   .then(res => {
+    //     res[currentCategory] = [...this.data.vendors[currentCategory], ...res[currentCategory]];
+    //     let vendors = {
+    //       ...this.data.vendors,
+    //       ...res
+    //     };
+    //     this.setData({
+    //       vendors
+    //     });
+    //     wx.hideNavigationBarLoading();
+    //   })
+    //   .catch(e => {
+    //     if (e === 400) {
+    //       this.setData({
+    //         [`bottomFlag.${currentCategory}`]: true
+    //       });
+    //     }
+    //     wx.hideNavigationBarLoading();
+    //   });
+  },
+  fetchList: function (category, skip, callback) {
     request(API.VENDOR.LISTS, METHOD.GET, {
-      locale: this.data.locale,
       realm: this.data.options.realm,
-      category: currentCategory,
-      start: this.data.cursor[currentCategory].start,
-      end: this.data.cursor[currentCategory].end
+      category,
+      skip,
+      limit: 10
     })
       .then(res => {
-        res[currentCategory] = [...this.data.vendors[currentCategory], ...res[currentCategory]];
-        let vendors = {
-          ...this.data.vendors,
-          ...res
-        };
+        if (res[category].length < 10)
+          this.setData({
+            [`bottomFlag.${category}`]: true
+          });
+        let vendors;
+        if (!this.data.vendors) {
+          vendors = res;
+        } else {
+          res[category] = !!this.data.vendors[category] ? [...this.data.vendors[category], ...res[category]] : res[category];
+          vendors = {
+            ...this.data.vendors,
+            ...res
+          };
+        }
         this.setData({
           vendors
         });
-        wx.hideNavigationBarLoading();
+        callback();
       })
       .catch(e => {
-        if (e === 400) {
-          this.setData({
-            [`bottomFlag.${currentCategory}`]: true
-          });
-        }
-        wx.hideNavigationBarLoading();
+        // if (e === 404) {
+        //   this.setData({
+        //     [`bottomFlag.${currentCategory}`]: true
+        //   });
+        // } else {
+        Toasts.requestFailed(Store.getState().global.locale);
+        // }
+        callback();
       });
+  },
+  search: function ({ event: { detail } }) {
+
+  },
+  filter: function () {
+
   },
   feedback
 })
