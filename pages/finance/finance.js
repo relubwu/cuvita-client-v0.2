91bbc2879b8a59e720e4a8fe54831c92e3251560
relubwu <bwu@ucsd.edu>
2019-08-28 23:18:18 +0800

    Change AD link src

--- pages/finance/finance.js
+++ pages/finance/finance.js
@@ -4,14 +4,12 @@ const { Store } = getApp();
 
 Page({
   onLoad: function () {
-    let { global: { locale }, page: { discovery: { services } } } = Store.getState();
+    let { locale } = Store.getState().global;
     wx.setNavigationBarTitle({
       title: LocalePackage.title[locale]
     });
-    for (let service of services)
-      if (service.name === 'finance')
-        this.setData({
-          src: service.options.src
-        });
+    this.setData({
+      src: options.src
+    });
   }
 })
--- pages/insurance/insurance.js
+++ pages/insurance/insurance.js
@@ -3,15 +3,13 @@ import * as LocalePackage from 'locale-package';
 const { Store } = getApp();
 
 Page({
-  onLoad: function () {
-    let { global: { locale }, page: { discovery: { services } } } = Store.getState();
+  onLoad: function (options) {
+    let { locale } = Store.getState().global;
     wx.setNavigationBarTitle({
       title: LocalePackage.title[locale]
     });
-    for (let service of services)
-      if (service.name === 'insurance')
-        this.setData({
-          src: service.options.src
-        });
+    this.setData({
+      src: options.src
+    });
   }
 })
