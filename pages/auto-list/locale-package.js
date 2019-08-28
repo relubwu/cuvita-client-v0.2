5998c4ff5d90a6229b1b2ce333de9ffce693bc42
relubwu <bwu@ucsd.edu>
2019-08-28 23:19:08 +0800

    Update Auto-list EOL

--- pages/auto-list/locale-package.js
+++ pages/auto-list/locale-package.js
@@ -1,1 +1,3 @@
-export const title = ['CarMax - 汽车商城', 'CarMax - Auto Superstore']
+export const title = ['CarMax - 汽车商城', 'CarMax - Auto Superstore'];
+export const pending = ['加载中', 'Loading'];
+export const eol = ['更多精彩请移步 CarMax.com', 'Visit CarMax.com for More'];
