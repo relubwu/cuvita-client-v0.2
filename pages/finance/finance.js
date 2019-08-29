3832140af4b562ca6524a64c4d3344f0bb968b6a
relubwu <bwu@ucsd.edu>
2019-08-29 18:47:38 +0800

    Bugfix

--- pages/finance/finance.js
+++ pages/finance/finance.js
@@ -3,7 +3,7 @@ import * as LocalePackage from 'locale-package';
 const { Store } = getApp();
 
 Page({
-  onLoad: function () {
+  onLoad: function (options) {
     let { locale } = Store.getState().global;
     wx.setNavigationBarTitle({
       title: LocalePackage.title[locale]
