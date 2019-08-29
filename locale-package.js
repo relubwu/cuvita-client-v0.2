347f9393c26f8ebd13d5b4c2c0df2ce9a7f55ae4
relubwu <bwu@ucsd.edu>
2019-08-29 18:46:52 +0800

    Append UX

--- locale-package.js
+++ locale-package.js
@@ -3,3 +3,4 @@ export const requestFailed = ['加载失败', 'Request Failed'];
 export const paymentFailed = ['支付失败', 'Payment Failed'];
 export const incompleteForm = ['请填写所有必填项', 'Please fill in all field(s)'];
 export const applying = ['应用更改中', 'Applying'];
+export const sorry = ['功能开发中', 'Unfinished Feature'];
