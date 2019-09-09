444c7aeb6403a3768c169f0542db2430ba19762c
relubwu <bwu@ucsd.edu>
2019-09-09 16:46:06 -0700

    Refactored

--- pages/article-detail/article-detail.js
+++ pages/article-detail/article-detail.js
@@ -1,11 +1,3 @@
-/**
- * CUVita Client Side Implementations - index.js
- * @scope /pages/article/detail
- * @author relubwu
- * @version 0.1.6
- * @copyright  © CHINESE UNION 2019
- */
-
 Page({
   onLoad: function ({ reference }) {
     this.setData({
