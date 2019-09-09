45e6373f1e6162a6e46b72a750aa03c1ede93d12
relubwu <bwu@ucsd.edu>
2019-09-09 16:48:27 -0700

    Refactored

--- app.js
+++ app.js
@@ -18,20 +18,14 @@ App({
       title: this.GlobalLocalePackage.loading[Store.getState().global.locale],
       mask: true
     });
-    // Synchronous core systemInfo
     Store.dispatch(GlobalActions.setLocale(wx.getStorageSync('locale')));
     Store.dispatch(GlobalActions.setSystemInfo(wx.getSystemInfoSync()));
-    // Acquire user token
     promisfy.login()
       .then(code => promisfy.fetch(`/dispatch/${ code }`))
       .then(({ data: { openid, user, member } }) => {
         Store.dispatch(GlobalActions.setUser({ openid, ...user }));
         wx.hideLoading();
-        if (!!member) {
-          Store.dispatch(GlobalActions.updateMember(member));
-        } else if (!!Store.getState().global.member) {
-          Store.dispatch(GlobalActions.purgeMember());
-        }
+        member ? Store.dispatch(GlobalActions.updateMember(member)) : Store.getState().global.member && Store.dispatch(GlobalActions.purgeMember());
       });
   }
 })
