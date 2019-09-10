2ac3a3e2370b4d0c9cd66f5188d0ff9b9dff4210
relubwu <bwu@ucsd.edu>
2019-09-10 12:19:21 -0700

    Refactored

--- pages/auto-list/auto-list.js
+++ pages/auto-list/auto-list.js
@@ -1,21 +1,21 @@
-import { request, METHOD } from '../../utils/promisfy';
-import * as API from '../../config/api.config';
-import * as LocalePackage from 'locale-package';
-import Palette from '../../config/palette.config';
 import feedback from '../../utils/feedback';
+import palette from '../../config/palette.config';
+import * as promisfy from '../../lib/wx.promisfy';
+import * as localePackage from 'locale-package';
 
-const { Store, GlobalLocalePackages } = getApp();
+const { Store, GlobalLocalePackage } = getApp();
+const limit = 10;
 
 Page({
-  data: {
-    LocalePackage,
-    Palette,
-    cursor: {},
+  data: { 
     bottomFlag: {},
-    current: 0
+    currentMake: { index: 0, alias: '' },
+    localePackage,
+    palette,
+    skip: {},
+    vehicles: {}
   },
   onLoad: function () {
-    // Synchronous storage hook
     let { locale, systemInfo, region } = Store.getState().global;
     this.setData({
       locale,
@@ -23,107 +23,60 @@ Page({
       region
     });
     wx.setNavigationBarTitle({
-      title: LocalePackage.title[this.data.locale]
+      title: localePackage.title[this.data.locale]
     });
     wx.showLoading({
-      title: GlobalLocalePackages.loading[this.data.locale]
+      title: GlobalLocalePackage.loading[this.data.locale]
     });
-    request(API.AUTO.MAKES, METHOD.GET, {})
-      .then(makes => {
+    promisfy.fetch(`/auto/makes`)
+      .then(({ data }) => {
         this.setData({
-          makes,
-          [`cursor.${makes[0].name}`]: {
-            skip: 0
-          }
+          makes: data,
+          ['currentMake.alias']: data[0].name
         });
-        this.fetchList(makes[0].name, 0, wx.hideLoading);
+        this.fetchList()
+          .then(() => {
+            wx.hideLoading();
+          });
       });
   },
-  mapStateToPage: function () {
-
-  },
-  onShow: function (options) {
-    this.unsubscribe = Store.subscribe(() => {
-      this.mapStateToPage();
-    });
-  },
-  onChange: function ({
-    detail: {
-      index
-    }
-  }) {
+  onChange: function ({ detail: { index } }) {
     feedback();
     this.setData({
-      current: index,
+      currentMake: { index, alias: this.data.makes[index].name },
       eol: false
     });
-    let currentMake = this.data.makes[index].name;
-    wx.showLoading({
-      title: GlobalLocalePackages.loading[this.data.locale]
-    });
-    if (!this.data.cursor[currentMake])
-      this.setData({
-        [`cursor.${ currentMake }`]: {
-          skip: 0
-        }
+    let { alias } = this.data.currentMake;
+    wx.showLoading({ title: GlobalLocalePackage.loading[this.data.locale] });
+    this.fetchList()
+      .then(length => {
+        length < limit && this.setData({ pending: false, [`bottomFlag.${ alias }`]: true, eol: true });
+        wx.hideLoading();
       });
-    this.fetchList(currentMake, 0, wx.hideLoading);
   },
   onReachBottom: function (e) {
-    let currentMake = this.data.makes[this.data.current].name;
-    if (this.data.cursor[currentMake].skip >= 20)
-      this.setData({
-        [`bottomFlag.${ currentMake }`]: true
-      });
-    if (this.data.bottomFlag[currentMake]) {
-      this.setData({
-        eol: true
-      });
+    let { alias } = this.data.currentMake;
+    if (this.data.bottomFlag[alias]) {
+      this.setData({ eol: true });
       return;
     }
-    this.setData({
-      pending: true,
-      eol: false
-    });
-    if (!!this.data.cursor[currentMake]) {
-      this.data.cursor[currentMake].skip += 10;
-    }
-    let { skip } = this.data.cursor[currentMake];
-    this.fetchList(currentMake, skip, () => { this.setData({ pending: false }) });
-  },
-  fetchList: function (make, skip, callback, keyword = '') {
-    request(API.AUTO.LISTS, METHOD.GET, {
-      make,
-      region: this.data.region,
-      skip
-    })
-      .then(res => {
-        if (res[make].length == 0)
-          return wx.showToast({
-            title: GlobalLocalePackages.requestNotFound[this.data.locale],
-            icon: 'none'
-          });
-        let cars;
-        if (!this.data.cars) {
-          cars = res;
-        } else {
-          res[make] = !!this.data.cars[make] ? [...this.data.cars[make], ...res[make]] : res[make];
-          cars = {
-            ...this.data.cars,
-            ...res
-          };
-        }
-        this.setData({
-          cars
-        });
-        callback();
-      })
-      .catch(e => {
-        wx.showToast({
-          title: GlobalLocalePackages.requestFailed[this.data.locale],
-          image: '/assets/icons/request-fail.png'
-        });
-        callback();
+    this.setData({ pending: true, eol: false });
+    this.fetchList()
+      .then(length => {
+        length < limit ? this.setData({ pending: false, [`bottomFlag.${ alias }`]: true, eol: true }) : this.setData({ pending: false });
       });
-  }
+  },
+  fetchList: function () {
+    let { realm, region, currentMake, skip, bottomFlag, vehicles } = this.data;
+    if (bottomFlag[currentMake]) return;
+    skip[currentMake.alias] === undefined ? this.setData({ [`skip.${currentMake.alias}`]: 0 }) : this.setData({ [`skip.${currentMake.alias}`]: this.data.skip[currentMake.alias] + limit });
+    return new Promise((resolve, reject) => {
+      promisfy.fetch(`/auto/lists/${ currentMake.alias }/${ region.alias }?limit=${ limit }&skip=${ skip[currentMake.alias] }`)
+        .then(({ data }) => {
+          vehicles[currentMake.alias] ? this.setData({ [`vehicles.${currentMake.alias}`]: vehicles[currentMake.alias].concat(data) }) : this.setData({ [`vehicles.${ currentMake.alias }`]: data });
+          resolve(data.length);
+        });
+    });
+  },
+  feedback
 })
