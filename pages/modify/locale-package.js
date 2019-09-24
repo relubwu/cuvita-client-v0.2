1ec818699abd017a593804da49de19df14c6d445
relubwu <bwu@ucsd.edu>
2019-09-23 18:11:34 -0700

    Bugfix iss#20
    the "school" field was prematurely amended to its display value (name) during the setData operations to inject autofiller data into form component and therefore caused validator rejecting form submit due to incompletence. This bug is fixed by reshaping field.school into an object containing both its "name" and "alias"

--- pages/modify/locale-package.js
+++ pages/modify/locale-package.js
@@ -6,9 +6,4 @@ export const title = ["会员信息", "Membership"];
 export const header = ["修改会员信息", "Information"];
 export const description = ["完善您的会员信息，以便VITA更好地为您提供服务", "Implement your membership detail"]
 export const submit = ["确认", "Update"];
-export const modal = {
-  success: {
-    title: ['提交成功', 'Success!'],
-    content: ['您的会员信息已成功更新', 'Your membership detail is updated']
-  }
-}
+export const success = ["您的会员信息已更新", "Your membership detail is updated"];
--- pages/modify/modify.js
+++ pages/modify/modify.js
@@ -36,21 +36,24 @@ Page({
       promisfy.fetch(`/school`)
     ])
       .then(res => {
-        let member = res[0].data;
-        let school = res[1].data;
+        let member = res[0];
+        let school = res[1];
         let fill = autofiller(['name', 'tel', 'email', 'school', 'gender', 'birthday'], member);
+        fill.school = { alias: fill.school };
         for (let index in school.list)
-          if (school.list[index].alias === fill.school) fill.school = school.list[index].name[this.data.locale];
+          if (school.list[index].alias === fill.school.alias) fill.school.name = school.list[index].name;
         this.setData({
-          ['fields[0][3].options']: school.matrix[this.data.locale],
-          ['fields[0][3].values']: school.values,
           ['fields[0][0].value']: fill.name,
           ['fields[0][1].value']: fill.tel,
           ['fields[0][2].value']: fill.email,
-          ['fields[0][3].label']: fill.school,
+          ['fields[0][3].value']: fill.school.alias,
+          ['fields[0][3].label']: fill.school.name[this.data.locale],
+          ['fields[0][3].options']: school.matrix[this.data.locale],
+          ['fields[0][3].values']: school.values,
+          ['fields[0][4].value']: fill.gender,
           ['fields[0][4].label']: [['男', '女', '其他'], ['Male', 'Female', 'Non-Binary']][this.data.locale][fill.gender],
-          ['fields[0][5].label']: new Date(fill.birthday).toLocaleDateString(),
           ['fields[0][5].value']: new Date(fill.birthday).getTime(),
+          ['fields[0][5].label']: new Date(fill.birthday).toLocaleDateString()
         });
         wx.hideLoading();
       });
@@ -58,8 +61,12 @@ Page({
   onSubmit: function ({ detail }) {
     wx.showLoading({ title: GlobalLocalePackage.loading[this.data.locale] });
     promisfy.post('/member/modify', { ...detail, openid: Store.getState().global.user.openid })
-      .then(({ data }) => {
+      .then(data => {
         wx.hideLoading();
+        wx.showToast({
+          title: localePackage.success[this.data.locale],
+          icon: "success"
+        });
       });
   }
 })
